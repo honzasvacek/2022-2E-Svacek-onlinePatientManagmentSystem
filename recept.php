ef814507708f1c718bc786b2a8444055ee6482b8
Jan Sváček <jan@svacek.eu>
2023-04-08 23:16:38 +0200

    8.3 - night

--- recept.php
+++ recept.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ůčet - výsledek</title>
+    <title>Recepty</title>
     <link rel="stylesheet" href="recept.css">
 </head>
 <body>
@@ -14,14 +14,52 @@
         public function zobrazeni_obsahu_receptu()
         {
             ?>
-            <div class="container">
-                <div class="row-1">
+            <div class="outer_container">
+                <div class="container">
+                    <div class="image-container">
+                        <div class="image"><p>Recept ICP - lékařský předpis</p><img src="Recepty/recept_leky.jpg" alt=""></div>
+                        <div class="image"><p>Poukaz na vyšetření/ oštření</p><img src="Recepty/Poukaz_vysos.PNG" alt=""></div>
+                        <div class="image"><p>Průkazka o zdravotní péči</p><img src="Recepty/prukazka_zdravotni_pece.jpg" alt=""></div>
+                        <div class="image"><p>Objednávka léku</p><img src="Recepty/objednavka_leku.jpg" alt=""></div>
+                        <div class="image"><p>Zdravotní záznam - stromatologie</p><img src="Recepty/stromatologie.jpg" alt=""></div>
+                        <div class="image"><p>Vyučtování výkonů v ambulantní péči</p><img src="Recepty/vyuctovani_ambpece.PNG" alt=""></div>
+                    </div>
 
-                </div>
-                <div class="row-2">
-
+                    <div class="popup-image">
+                        <span>&times;</span> <!-- html entita, která vytvoří symbol křížku -->
+                        <img src="Recepty/recept_leky.jpg" alt="">
+                        <button id="btn-tisk" class="btn-tisk" >Tisk</button>
+                    </div>
                 </div>
             </div>
+
+            <script>
+                document.querySelectorAll('.image-container img').forEach(image =>
+                {
+                    image.onclick = () =>
+                    {
+                        document.querySelector('.popup-image').style.display = 'block';
+                        document.querySelector('.popup-image img').src = image.getAttribute('src');
+                    }
+                });
+
+                //nastavím spanu, což je křížek, akci on
+                document.querySelector('.popup-image span').onclick = () =>
+                {
+                    document.querySelector('.popup-image').style.display = 'none';
+                }
+
+                //tlačítko na tisk
+                const btn_tisk = document.getElementById('btn-tisk'); //uložím si tlačítko do proměnné
+
+                //nastavím co se stane když na tlačítko někdo klikne
+                btn_tisk.addEventListener('click', function() 
+                {
+                    print(); //vytisknu obsah
+                });
+
+            </script>
+
             <?php
         }
     }
